6121a5a246d30fce59931dca5d0fe12e97031755
Cedrik Neumann <cedrik.neumann@wooga.net>
2017-06-15 14:30:13 +0200

    fix(setup.py): add karajan package

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.1',
     url='https://github.com/wooga/bit.karajan',
     license='',
+    packages=['karajan'],
     author='Wooga Business Intelligence Team',
     author_email='bit-admin@wooga.com',
     description='A conductor of aggregations in Apache Airflow',
