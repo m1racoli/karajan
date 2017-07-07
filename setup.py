37cdab01b1b8ad3759965cc5e91106117d93d4fa
Cedrik Neumann <cedrik.neumann@wooga.net>
2017-07-07 12:00:23 +0200

    chore(setup): bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='karajan',
-    version='0.0.12',
+    version='0.0.13',
     url='https://github.com/wooga/bit.karajan',
     license='',
     packages=['karajan'],
