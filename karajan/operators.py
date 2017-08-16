8c891d57a399c074b934435a3981e9eb9a154448
Cedrik Neumann <cedrik.neumann@wooga.net>
2017-08-16 10:12:03 +0200

    cleanup

--- karajan/operators.py
+++ karajan/operators.py
@@ -36,11 +36,9 @@ class KarajanBaseOperator(BaseOperator):
             ds_start = ds_start if (self.aggregation.reruns + self.aggregation.offset == 0) else ds_start - timedelta(
                 days=self.aggregation.reruns + self.aggregation.offset)
             ds_end = ds_end if self.aggregation.offset == 0 else ds_end - timedelta(days=self.aggregation.offset)
-        ds_start = ds_start.strftime("%Y-%m-%d")
-        ds_end = ds_end.strftime("%Y-%m-%d")
-        self.params['start_date'] = ds_start
-        self.params['end_date'] = ds_end
-        return ds_start, ds_end
+        self.params['start_date'] = ds_start.strftime("%Y-%m-%d")
+        self.params['end_date'] = ds_end.strftime("%Y-%m-%d")
+        return self.params['start_date'], self.params['end_date']
 
 
 class KarajanAggregateOperator(KarajanBaseOperator):
