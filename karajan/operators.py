132d324cf7e41d61015404366d6b0a99495ea6db
Cedrik Neumann <cedrik.neumann@wooga.net>
2017-09-06 11:56:11 +0200

    fix(Operators): use get for safe value retrieval

--- karajan/operators.py
+++ karajan/operators.py
@@ -51,7 +51,7 @@ class KarajanBaseOperator(BaseOperator):
         if conf is None:
             return None
         limit = conf.get('limit')
-        return limit and any(not limit[name] for name in names)
+        return limit and any(not limit.get(name) for name in names)
 
     @staticmethod
     def limited_columns(context, name, columns):
