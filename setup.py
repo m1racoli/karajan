e7a04e814edfb9764925b4598a57fe8a829588a8
Cedrik Neumann <cedrik.neumann@wooga.net>
2017-12-21 12:02:51 +0100

    chore(License): add MIT license

--- setup.py
+++ setup.py
@@ -1,13 +1,34 @@
+#
+# Copyright 2017 Wooga GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from setuptools import setup
 
 setup(
     name='karajan',
-    version='0.3.23',
-    url='https://github.com/wooga/bit.karajan',
+    version='0.4.0',
+    url='https://github.com/wooga/karajan',
     license='',
     packages=['karajan', 'karajan.bin'],
-    author='Wooga Business Intelligence Team',
-    author_email='bit-admin@wooga.com',
+    author='Cedrik Neumann',
+    author_email='cedrik.neumann@outlook.com',
     description='A conductor of aggregations in Apache Airflow',
     scripts=['karajan/bin/karajan'],
     install_requires=[
